d6b697345f51dd692336fe8d4d7da7450adf50c8
Seokyunghwan <dhgh8875@icloud.com>
2024-12-14 15:19:30 +0900

    good

--- index.js
+++ index.js
@@ -142,18 +142,20 @@ $(document).ready(function(){
 
     const News01Swiper = new Swiper('.news01-content.swiper',{
         loop:true,
-        slidesPerView: 5,
-        
+        spaceBetween:10,
         navigation: {
             nextEl: '.news01-content .next',
             prevEl: '.news01-content .prev',
         },
 
         breakpoints:{
-            970: {
+            1200: {
                 slidesPerView:3
             },
-            580:{
+            768:{
+                slidesPerView:2
+            },
+            0:{
                 slidesPerView:1
             }
         }
